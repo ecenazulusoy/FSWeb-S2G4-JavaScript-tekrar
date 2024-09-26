e860af1be6c653be3d0aefceb7e03b0c0ea76df2
ecenazulusoy <ulusoyecenaz@gmail.com>
2024-09-26 18:32:48 +0300

    7 test passed

--- index.js
+++ index.js
@@ -50,8 +50,8 @@ function KareninAlani(kenaruzunlugu) {
 	4. Hesaplanan çemberin çevresi döndürülecektir.
 */
 
-function CemberinCevresi(/* kodlar buraya */) {
-  /* kodlar buraya */
+function CemberinCevresi(yaricap) {
+  return 2 * pi * yaricap
 }
 
 /* (Oto test yok) Yukarıdaki CemberinCevresi fonksiyonunu yarıçap = 5 vererek aşağıda çalıştırıp, sonucu konsolda gözlemleyin (console.log)  */
@@ -64,8 +64,8 @@ function CemberinCevresi(/* kodlar buraya */) {
 	4. Hesaplanan çemberin alanı döndürülecektir.
 */
 
-function CemberinAlani(/* kodlar buraya */) {
-  /* kodlar buraya */
+function CemberinAlani(yaricap , pi) {
+  return pi * yaricap * yaricap 
 }
 
 /* (Oto test yok) Yukarıdaki CemberinAlani fonksiyonunu yarıçap = 15 vererek aşağıda çalıştırıp, sonucu konsolda gözlemleyin (console.log)  */
@@ -90,7 +90,7 @@ function CemberinAlani(/* kodlar buraya */) {
 /*  (oto test yok) sayilar dizisi içinde kaç adet sayı olduğunu konsola yazdırın */
 
 let ucetambolunenler,
-  enkucuk,
+  enkucuk, 
   enbuyuk,
   ucebolunenlerintoplami,
   besyuzdenkucuksayilar,
@@ -98,7 +98,16 @@ let ucetambolunenler,
   tekraredensayilar;
 
 // 3a çözümü
-
+enkucuk = sayilar[0];
+enbuyuk = sayilar[0];
+for (let i = 0; i< sayilar.length; i++) {
+ if (sayilar[i] < enkucuk) {
+  enkucuk = sayilar[i]
+ } ;
+ if (sayilar[i] > enbuyuk) {
+  enbuyuk = sayilar[i]
+ };
+}
 /* kodlar buraya */
 
 // 3b çözümü:
@@ -110,13 +119,13 @@ let ucetambolunenler,
 /* kodlar buraya */
 
 // 3d çözümü
-
-/* kodlar buraya */
+besyuzdenkucuksayilar = sayilar.filter( (sayi) => {
+  return sayi < 500 
+} )
 
 // 3e çözümü
 
-/* kodlar buraya */
-
+ siralisayilar = besyuzdenkucuksayilar.sort((a,b) => a-b) ;
 // 3f çözümü
 
 /* kodlar buraya */
